b7bfda47720bac276e588ffa7a1aba9581c40001
hp0404 <hryhorii.pavlenko@gmail.com>
2021-02-03 10:51:27 +0200

    topic presence figures

--- R/timeseries_weights.r
+++ R/timeseries_weights.r
@@ -9,35 +9,19 @@ library(ggpubr)
 library(ggalt)
 library(zoo)
 
+
 #import data 
-df <- read.csv("../data/processed/year_topic_weights2.csv", header = TRUE)
-df2 <- df[df$topic_label=="Domestic politics" | df$topic_label=="Diplomacy and trade" | df$topic_label=="Economy"
-          | df$topic_label=="Nationalism" | df$topic_label=="Military/Defense", ]
-df3 <- df[df$topic_label=="Oil and energy" | df$topic_label=="Regional politics" | df$topic_label=="Crime and punishment"
-          | df$topic_label=="Sport and culture" | df$topic_label=="Human development", ]
+#df <- read.csv("../data/processed/topics_over_time.csv")
+df <- read.csv("../data/processed/topics_over_time_top.csv")
 
 #find median topic weight
-median(df$average_weight)
+median(df$weight)
 #.08
 
-
-#plot
-plot1 <- ggplot(df2, aes(x = year, y = average_weight, colour = topic_label)) +
+ggplot(df, aes(x = year, y = weight, colour = label)) +
   geom_smooth() +
   geom_hline(yintercept = 0.08, size = 1, colour="#333333") +
-  labs(title = "Putin Corpus Top-5 Topic Presence (1999 - 2020)",
+  labs(title = "Russian Corpus Top-5 Topic Presence (2000 - 2020)",
        subtitle = "Black line represents median topic weight through time") +
   xlab("Date") + ylab("Average Yearly Topic Weight") + 
   theme_hc()
-
-plot1
-
-plot2 <- ggplot(df3, aes(x = year, y = average_weight, colour = topic_label)) +
-  geom_smooth() +
-  geom_hline(yintercept = 0.08, size = 1, colour="#333333") +
-  labs(title = "Putin Corpus Bottom-5 Topic Presence (1999 - 2020)",
-       subtitle = "Black line represents median topic weight through time") +
-  xlab("Date") + ylab("Average Yearly Topic Weight") + 
-  theme_hc()
-
-plot2
