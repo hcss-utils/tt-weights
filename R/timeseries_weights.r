d426e6f6aebc6e6e796345c78b7598257ee5a5d6
hp0404 <hryhorii.pavlenko@gmail.com>
2021-02-21 11:50:46 +0200

    tableau pallete and svg format

--- R/timeseries_weights.r
+++ R/timeseries_weights.r
@@ -8,20 +8,32 @@ library(scales)
 library(ggpubr)
 library(ggalt)
 library(zoo)
-
+library(svglite)
 
 #import data 
-#df <- read.csv("../data/processed/topics_over_time.csv")
-df <- read.csv("../data/processed/topics_over_time_top.csv")
+setwd("C:/github/tt-weights/R")
+df <- read.csv("../data/processed/topics_over_time.csv")
+#df <- read.csv("../data/processed/topics_over_time_top.csv")
 
 #find median topic weight
 median(df$weight)
 #.08
 
-ggplot(df, aes(x = year, y = weight, colour = label)) +
+unique(df$label)
+
+
+
+p <- ggplot(df, aes(x = year, y = weight, colour = factor(label))) +
   geom_smooth() +
-  geom_hline(yintercept = 0.08, size = 1, colour="#333333") +
-  labs(title = "Russian Corpus Top-5 Topic Presence (2000 - 2020)",
-       subtitle = "Black line represents median topic weight through time") +
+  #geom_hline(yintercept = 0.08, size = 1, colour="#333333") +
+  labs(title = "Russian Corpus Topic Presence (2000 - 2020)", #"Russian Corpus Top-5 Topic Presence (2000 - 2020)"
+       #subtitle = "Black line represents median topic weight through time",
+       color="Topic name") +
   xlab("Date") + ylab("Average Yearly Topic Weight") + 
-  theme_hc()
+  theme_hc() 
+
+p <- p + scale_colour_tableau("Classic 10")
+
+p
+
+ggsave(file="../reports/figures/selected_topics.svg", plot=p, width=18, height=9)
